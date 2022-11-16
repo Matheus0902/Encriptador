d545f1e36f2ac38f4ff8b09616cf0d05956cc7d1
Matheus Silva <matheus090298@hotmail.com>
2022-11-16 19:30:07 -0300

    Refatorando código js

--- js/challeng.js
+++ js/challeng.js
@@ -1,90 +1,77 @@
-function pulaLinha() {
-        document.write("<br><br>");
+const text = document.querySelector('.text');
+
+text.addEventListener('keypress', function(e){
+
+    if(!verificChar(e)) {
+        e.preventDefault();
     }
-
-    function mostra(frase) {
-        document.write(frase);
-        pulaLinha();
-    } 
-var text = document.querySelector('.text');
-
-    text.addEventListener('keypress', function(e){
-
-        if(!verificaChar(e)) {
-            e.preventDefault();
-        }
-    })
+})
     
-    function verificaChar(e) {
+function verificChar(e) {
     
-        const char = String.fromCharCode(e.keyCode);
+    const char = String.fromCharCode(e.keyCode);
         
-        const padrão = '[a-z]';
+    const padrão = '[a-z]';
     
-        if(char.match(padrão)){
-            console.log(char);
-            return true; 
-        }
+    if(char.match(padrão)){
+        console.log(char);
+        return true; 
     }
+}
 
-   function copiarTexto(){
-        
+const textOne = document.getElementById('textOne');
+textOne.focus();
+
+const textTwo = document.getElementById('textTwo')
+
+/* Função para copiar o texto criptografado/descriptografado */
+
+function copyText(){
+
+    textTwo.select();
+    textTwo.setSelectionRange(0, 99999);
+
+    document.execCommand("copy");
+    textTwo.style.backgroundImage= "url('./assets/rindo.png')";
+    textTwo.value = "";  
+}
+
+const btCopy = document.getElementById('btCopy');
+btCopy.onclick = copyText;
+
+/* Função para mostrar o texto criptografado */
     
-        var textoCopiado = document.getElementById('textTwo')
-        textoCopiado.select();//para selecionar o texto
-        textoCopiado.setSelectionRange(0, 99999); //para dispositivos moveis
-    
-        document.execCommand("copy"); //vai copiar o texto selecionado
-        inputRecebido.style.backgroundImage= "url('rindo.png')";
-        inputRecebido.value = "";
+function playEncryptedText(){
        
+    const textReceived = textOne.value;
+    let textoCripitografado = textReceived.replaceAll(/e/gi, "enter").replaceAll(/i/gi, "imes").replaceAll(/a/gi, "ai").replaceAll(/o/gi, "ober").replaceAll(/u/gi, "ufat");
         
-    }
-    
-    var inputEnviado = document.getElementById('textOne');
-    inputEnviado.focus();
+    textTwo.value = textoCripitografado;
+    textTwo.style.backgroundImage= "none";
 
-    var inputRecebido = document.getElementById('textTwo')
-    
-    var button = document.getElementById('btEncrypt');
-    button.onclick = mostraTextoCriptografado;
+    textOne.value = "";
+    textOne.focus();
+}
 
-    var button2 = document.getElementById('btDecrypt');
-    button2.onclick = mostraTextoDescripitografado;
-   
-    var copiar = document.getElementById('btCopy');
-    copiar.onclick = copiarTexto;
+const btEncrypt = document.getElementById('btEncrypt');
+btEncrypt.onclick = playEncryptedText;
 
+/* Função para mostrar o texto descriptografado */
 
+function playDecryptedText(){
 
-    function mostraTextoCriptografado(texto){
-       
-        var textoRecebido = inputEnviado.value;
-        textoCripitografado = textoRecebido.replaceAll(/e/gi, "enter").replaceAll(/i/gi, "imes").replaceAll(/a/gi, "ai").replaceAll(/o/gi, "ober").replaceAll(/u/gi, "ufat");
-        //alert(textoCripitografado);/// exibirá a mensagem recebida do input
+    const textReceived = textOne.value;
+    let textoDecrypted = textReceived.replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ai", "a").replaceAll("ober", "o").replaceAll("ufat", "u");
         
-        inputRecebido.value = textoCripitografado;
-
-        inputEnviado.value = "";
-        inputEnviado.focus();
-        inputRecebido.style.backgroundImage= "none"; //quando a função for executada a imagem do texto recebido sumirá.
-        }
-
-    
-
-    function mostraTextoDescripitografado(texto){
-
-        var textoRecebido = inputEnviado.value;
-        textoDescripitografado = textoRecebido.replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ai", "a").replaceAll("ober", "o").replaceAll("ufat", "u");
-        //alert(textoDescripitografado);/// exibirá a mensagem recebida do input
+    textTwo.value = textoDecrypted;
+    textTwo.style.backgroundImage= "none";  
         
-        inputRecebido.value = textoDescripitografado;
-        
-        inputEnviado.value = "";
-        inputEnviado.focus()
-        inputRecebido.style.backgroundImage= "none"; 
-        
-    }
+    textOne.value = "";
+    textOne.focus() 
+}
+
+const btDecrypt = document.getElementById('btDecrypt');
+btDecrypt.onclick = playDecryptedText;
 
    
 
